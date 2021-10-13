d161142b84f3399c75bb65c60ccbbc01423c4018
ManavJain2210 <54030942+ManavJain2210@users.noreply.github.com>
2021-10-13 14:01:19 +0530

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -42,12 +42,12 @@ def load_data(mode="train"):
         if "LJ" in hp.data:
             # Parse
             fpaths, text_lengths, texts = [], [], []
-            transcript = os.path.join(hp.data, 'transcript.csv')
+            transcript = os.path.join(hp.data, 'metadata.csv')
             lines = codecs.open(transcript, 'r', 'utf-8').readlines()
             for line in lines:
                 fname, _, text = line.strip().split("|")
 
-                fpath = os.path.join(hp.data, "wavs", fname + ".wav")
+                fpath = os.path.join(hp.data, "wav", fname + ".wav")
                 fpaths.append(fpath)
 
                 text = text_normalize(text) + "E"  # E: EOS
